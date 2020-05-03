582fc090969bfe903d983f67d5a33a0cca2ffbdd
leandro-svg <leandro.dibella@gmail.com>
2020-05-03 13:37:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import Antenna 
-import Wall 
-import Space 
+import Antenna
+import Wall
+import Space
 import Ray
 import numpy as np
 import random
 import map
-if __name__ == "__main__" : 
-    
-    Walls = [  
+if __name__ == "__main__" :
+
+    Walls = [
         Wall.wall(0.3, 5, 1, 0.014, np.array([30, 0]), np.array([0, 0])),
         Wall.wall(0.3, 5, 1, 0.014, np.array([0, 0]), np.array([0, 15])),
         Wall.wall(0.3, 5, 1, 0.014, np.array([30, 0]), np.array([30, 15])),
+        Wall.wall(0.3, 5, 1, 0.014, np.array([30, 15]), np.array([0, 15])),
         Wall.wall(0.3, 5, 1, 0.014, np.array([30, 15]), np.array([0, 15]))]
-        
-    Tx = [(Antenna.Antenna(np.array([12,14]), 0.1, []))]##,(Antenna.Antenna(np.array([6, 6]), 100, []))] 
+
+
+    Tx = [(Antenna.Antenna(np.array([12,14]), 0.1, []))]##,(Antenna.Antenna(np.array([6, 6]), 100, []))]
     txs = []
     listeTx = []
-    
+
     for tx in (Tx):
         txs = tx._pos
         listeTx += [txs]
@@ -29,33 +31,33 @@ if __name__ == "__main__" :
                if ([i/2,j/2] != [x[0],x[1]]):
                    rx =  (Antenna.Antenna(np.array([i/2,j/2]), 0, []))
                    Rx.append(rx)
-                   
+
            rx= 0
-    #Rx = [(Antenna.Antenna(np.array([25, 5]), 0, []))] 
-    
+    #Rx = [(Antenna.Antenna(np.array([25, 5]), 0, []))]
+
     env = Space.Space(Walls, Tx, Rx)
     rays = env.Predict(4)
     #env.save();
     #env = pickle.load(open("save.pickle", "rb"))
     Rayon = []
     listeRayon = []
-    for ray in (rays) : 
+    for ray in (rays) :
          Rayon = ray.Coordinates
          listeRayon+=(Rayon)
          Rayon = []
-         
+
     wall = []
     listWall = []
     for mur in (Walls):
         wall = [mur.Vec1, mur.Vec2]
         listWall += [wall]
         wall = []
-        
-    
-    
+
+
+
     map = map.map()
-    
-    
+
+
     listeRx = []
     for rx in (Rx):
         try :
@@ -66,6 +68,6 @@ if __name__ == "__main__" :
         rxs = [rx._pos, power]
         listeRx += [rxs]
         rxs = []
-    
-    
+
+
     map.drawing(listeRayon, listeTx, listeRx, listWall, rays)
